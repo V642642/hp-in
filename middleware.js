70d72ed1b5209288ab083a413f3571b026ef4877
V642642 <83902292+V642642@users.noreply.github.com>
2023-07-21 10:43:28 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   
-  if (req.ip.geoip.country === "CA") {
+  if (req.geo.country === "CA") {
     return NextResponse.redirect(process.env.NEXT_PUBLIC_CA_URL);
-  } else if (req.ip.geoip.country === "IN") {
+  } else if (req.geo.country === "IN") {
     return NextResponse.redirect(process.env.NEXT_PUBLIC_IN_URL);
   } else {
     return NextResponse.next();
